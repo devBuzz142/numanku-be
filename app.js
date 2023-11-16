dfbbe038108ef73ded4d55139d6baba8300361f0
devBuzz142 <devbuzz142@gmail.com>
2023-11-16 23:45:50 +0900

    feat: conn to mysql db

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  connectDB();
+app.listen(port, async () => {
   console.log(`Example app listening at http://localhost:${port}`);
+
+  const conn = await connectDB();
+
+  conn.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  conn.end((err) => {
+    if (err) throw err;
+    console.log("Closed!");
+  });
 });
--- db.js
+++ db.js
@@ -1,22 +1,16 @@
-const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mariadb = require("mysql");
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      "mongodb://root:root@" + process.env.MONGO_URI,
-      {
-        // These are just to get rid of deprecation warnings
-        useNewUrlParser: true,
-      }
-    );
-    console.log("MongoDB connected!");
+  const conn = await mariadb.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
 
-    // close
-    await conn.connection.close();
-    console.log("MongoDB connection closed!");
-  } catch (err) {
-    console.log(err);
-  }
+  return conn;
 };
 
 module.exports = connectDB;
