af0c7bb45c6175191835c68c2102c1921c9f66bb
devBuzz142 <devbuzz142@gmail.com>
2023-11-18 02:39:38 +0900

    feat: cors

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
